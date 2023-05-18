9bc6fc1093d9759374ac311c82cd0df9b2a94be1
Vijey Anbarasan <vijey.anbarasan@nulogic.io>
2023-05-18 12:53:36 +0530

    updated the logic to fetch donationDetails from using 'Index' to 'items()' method.

--- donation-sku-configuration/donations/app/modules/fetch_site_titles/operations.py
+++ donation-sku-configuration/donations/app/modules/fetch_site_titles/operations.py
@@ -2,15 +2,20 @@ from donations.app.lib.dbutils import destination_collection
 
 # in a doctionary where the key is the "Sites" string and the value is where there is only three key "com","ca"and "outlet-com" and the value is donationDetails" field "com","ca","outlet-com" key 
 # which is to be fetched from destination_collection where "productVariantId:'sku30042'"
-def fetch_donation_details_by_site():
+def fetch_donationDetails_sites():
     try:
         donation_details = {}
         product_variant = destination_collection.find_one({'productVariantId': 'sku30042'})
         if product_variant and 'donationDetails' in product_variant:
             donation_details = product_variant['donationDetails']
-            com_key = list(donation_details.keys())[0]
-            ca_key = list(donation_details.keys())[1]
-            outlet_key = list(donation_details.keys())[2]
+            for keys in donation_details.items():
+                for i in keys:
+                    if i == 'com':
+                        com_key = i
+                    elif i == 'outlet-com':
+                        ca_key = i
+                    elif i == 'ca':
+                        outlet_key = i
             donation_details_by_site = {'Sites': {'com': com_key, 'outlet-com': ca_key, 'ca': outlet_key}}
             return donation_details_by_site
         else:
@@ -18,7 +23,6 @@ def fetch_donation_details_by_site():
     except Exception as e:
         print(f"Error fetching donation details by site: {str(e)}")
         return None
-
-donation_details_by_site = fetch_donation_details_by_site()
+donation_details_by_site = fetch_donationDetails_sites()
 print("----------------------GET SITE TITLES-----------------------")
 print (donation_details_by_site)
--- donation-sku-configuration/handler.py
+++ donation-sku-configuration/handler.py
@@ -1,6 +1,6 @@
 from donations.app.modules.fetch_donation_titles_and_details.operations import fetch_donation_sku_details
-from donations.app.modules.fetch_site_titles.operations import fetch_donation_details_by_site
+from donations.app.modules.fetch_site_titles.operations import fetch_donationDetails_sites
 
 def process(event, context):
-    fetch_donation_details_by_site
+    fetch_donationDetails_sites
     fetch_donation_sku_details
